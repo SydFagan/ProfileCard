fc59821b0e3234d5096db234477643c5cfd4106d
SydFagan <syfagan@iu.edu>
2026-09-08 21:42:19 -0400

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,85 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+//import { Image } from 'expo-image';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
-
+//import { HelloWave } from '@/components/hello-wave';
+//import ParallaxScrollView from '@/components/parallax-scroll-view';
+//import { ThemedText } from '@/components/themed-text';
+//import { ThemedView } from '@/components/themed-view';
+//import { Link } from 'expo-router';
+/*make my pfp bald me*/
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
+    <View style={styles.container}>
+    <View style={styles.card}>
+      
+       <Image
+                source={require('@/assets/images/Pfp.jpeg')}
+                style={{ width: 200, height: 200, alignSelf: 'center',
+                  borderRadius: 30,
+                 }}
               />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  <Text style={styles.name}>Sydney Fagan</Text>
+    <View style={styles.facts}>
+ <Text style={styles.bio}> It's nice to meet you! A little about me: I'm a senior at IU Indianapolis, majoring in MAS and minoring in Informatics. I love to read, play video games, and spend time with my cats. I also enjoy learning new things and practicng my barista skills.
+  </Text>
+   <Text style={styles.fact}>• Every year I go white water kayaking in Tennessee with my dad! It's been a tradition since I was little.</Text>
+   <Text style={styles.fact}>• In my free time I help out at my local stables in exchange for riding lessons. I one day want to have my own little farm. </Text>
+    <Text style={styles.fact}>• I also do a ton of baking, my parents and boyfriend are my personal taste testers. Especially before the treats disperse among friends and coworkers. </Text>
+    <Text style={styles.fact}>• I'm not actually bald! </Text>
+</View>
+      </View>
+            </View>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+facts: {
+width:"100%",
+gap: 5,
+
+},
+
+fact: {
+fontSize: 15,
+
+},
+
+bio: {
+fontSize: 16,
+textAlign: 'center',
+marginBottom:15,
+},
+
+container: {
+  flex: 1,
+  backgroundColor: '#d5d5c5',
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+
+  name: {
+    color: '#666',
+    marginBottom:10,
+    fontSize: 24,
+    fontWeight: 'bold',
+
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  
+card: {
+  width:300,
+  backgroundColor: '#fff',
+  padding:20,
+  borderRadius:15,
+  alignItems: 'center',
+},
+
+profileImage: {
+  width:120,
+  height:120,
+  borderRadius:60,
+  marginBottom:15,
+
+},
+
 });
